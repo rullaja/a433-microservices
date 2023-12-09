05809282a9417d72e986d86540c7b3ecc4086343
rullaja <nurulnur061203@gmail.com>
2023-12-09 23:36:40 +0700

    submission fix

--- build_push_images.sh
+++ build_push_images.sh
@@ -1,10 +1,15 @@
 # Perintah untuk membuat Docker image dari Dockerfile dengan nama image item-app dan tag v1
 docker build -t item-app:v1 .
-
+ 
 # Melihat daftar image di lokal
 docker images
-
+ 
+# Menyimpan username dalam variabel
+username="nurulnur126"
+ 
+# Perintah untuk login ke Docker Hub secara interaktif
+docker login -u $username
+ 
 # Mengunggah image ke Docker Hub
-cat ../dockerpass.txt | docker login --username nurulnur126 --password-stdin
 docker tag item-app:v1 nurulnur126/item-app:v1
 docker push nurulnur126/item-app:v1
